7e8cb1db9eb026e89a755d476dc2f41e4c60e2f1
Konrad Skowron <73618275+Konrad-Skowron@users.noreply.github.com>
2023-07-07 22:32:25 +0200

    Added access token expiration handling

--- background.js
+++ background.js
@@ -1,73 +1,108 @@
+const clientId = '2ujezphm87cg7ii7d8jvu8if66wwe7';
+const clientSecret = 'g35oxxgt6jhohutf4fh6come760640';
+let accessToken = '';
+let url = 'https://www.twitch.tv/xqc';
+
 chrome.alarms.create({ periodInMinutes: 1 });
 chrome.alarms.onAlarm.addListener(isLive);
 
 async function isLive() {
-  const twitchLiveStatus = await isLiveOnTwitch();
   const kickLiveStatus = await isLiveOnKick();
+  const twitchLiveStatus = await isLiveOnTwitch();
 
-  if (!twitchLiveStatus && !kickLiveStatus) {
+  if (!kickLiveStatus && !twitchLiveStatus) {
     setTitleOffline();
   }
 }
 
-async function isLiveOnTwitch() {
-  const response = await fetch('https://api.twitch.tv/helix/streams?user_login=xqc', {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer l5lb7e7jvg8v4ern55srcl96m2d8ay',
-      'Client-ID': '2ujezphm87cg7ii7d8jvu8if66wwe7'
+async function isLiveOnKick() {
+  try {
+    const response = await fetch('https://kick.com/api/v2/channels/xqc');
+    if (!response.ok) {
+      throw new Error(`Error! Status: ${response.status}`);
     }
-  });
-  if (!response.ok) {
-    throw new Error(`Error! Status: ${response.status}`);
-  }
-  const data = await response.json();
-  const streamData = data.data[0];
-  if (streamData && streamData.type === 'live') {
-    setTitleLive('#eb0400', streamData.title, streamData.game_name, streamData.viewer_count);
-    url = 'https://www.twitch.tv/xqc';
-    return true;
+    const streamData = await response.json();
+    if (streamData?.livestream !== null) {
+      setTitleLive('#53fc18', streamData.livestream.session_title, streamData.livestream.categories[0].name, streamData.livestream.viewer_count);
+      url = 'https://www.kick.com/xqc';
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
   }
   return false;
 }
 
-async function isLiveOnKick() {
-  const response = await fetch('https://kick.com/api/v2/channels/xqc');
-  if (!response.ok) {
-    throw new Error(`Error! Status: ${response.status}`);
-  }
-  const data = await response.json();
-  const streamData = data;
-  if (streamData && streamData.livestream !== null) {
-    setTitleLive('#53fc18', streamData.livestream.session_title, streamData.livestream.categories[0].name, streamData.livestream.viewer_count);
-    url = 'https://www.kick.com/xqc';
-    return true;
+async function isLiveOnTwitch() {
+  try {
+    const response = await fetch('https://api.twitch.tv/helix/streams?user_login=xqc', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Client-ID': clientId
+      }
+    });
+    if (!response.ok) {
+      if (response.status === 401) {
+        getTwitchToken();
+        return false;
+      }
+      throw new Error(`Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    const streamData = data.data[0];
+    if (streamData && streamData.type === 'live') {
+      setTitleLive('#eb0400', streamData.title, streamData.game_name, streamData.viewer_count);
+      url = 'https://www.twitch.tv/xqc';
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
   }
   return false;
+}
+
+async function getTwitchToken() {
+  try {
+    const response = await fetch('https://id.twitch.tv/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
+    });
+    if (!response.ok) {
+      throw new Error(`Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    accessToken = data.access_token;
+  } catch (error) {
+    console.log(error);
+  }
+  isLive();
 }
 
 function setTitleLive(color, title, game, viewers) {
   chrome.action.setBadgeText({
-    text: 'LIVE'
+    text: 'LIVE',
   });
   chrome.action.setBadgeBackgroundColor({
-    color: color
+    color: color,
   });
   chrome.action.setTitle({
-    title: `xQc\n${title}\n${game}\n${viewers} 🤓`
+    title: `xQc\n${title}\n${game}\n${viewers} 🤓`,
   });
 }
 
 function setTitleOffline() {
   chrome.action.setBadgeText({
-    text: ''
+    text: '',
   });
   chrome.action.setTitle({
-    title: 'xQc\nOFFLINE'
+    title: 'xQc\nOFFLINE',
   });
 }
 
-let url = 'https://www.twitch.tv/xqc';
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url });
 });
